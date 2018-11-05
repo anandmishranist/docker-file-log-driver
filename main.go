f2c65b4172dc684d2804d1ac3759e980574e0df5
Anand Mishra <anand.mishra@nist.gov>
2018-11-05 11:19:08 -0500

    package changes
    Signed-off-by: Anand Mishra <anand.mishra@nist.gov>

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/docker/go-plugins-helpers/sdk"
-	"github.com/deep-compute/docker-file-log-driver/driver"
-	"github.com/deep-compute/docker-file-log-driver/http"
+	"github.com/anandmishranist/docker-file-log-driver"
+	"github.com/anandmishranist/docker-file-log-driver"
 )
 
 var logLevels = map[string]logrus.Level{
